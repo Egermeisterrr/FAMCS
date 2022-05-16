36057c21ba98431d8b9aead9f9fb7a2ca55480f4
Egor <102251036+Egermeisterrr@users.noreply.github.com>
2022-05-16 17:40:51 +0300

    update the function "solution"
    add comparator

--- Data Sructures/Main.kt
+++ Data Sructures/Main.kt
@@ -3,35 +3,55 @@ import java.io.FileOutputStream
 import java.io.PrintStream
 import java.util.*
 
-class Vertex(var x: Int, var y: Int, var mark: Long, var height: Int)
+class Vertex(var x: Int, var y: Int, var mark: Int, var height: Int)
 
-fun solution() : Int {
-   // code for calculating the minimum path
-   return 0
+fun solution(
+    n: Int,
+    m: Int,
+    matrix: Array<Array<Int>>,
+    xStart: Int,
+    yStart: Int,
+    xEnd: Int,
+    yEnd: Int,
+    k: Int
+): Int {
+    val comparator: Comparator<Vertex> = Comparator<Vertex> { o1, o2 ->
+       if (o1.mark < o2.mark) {
+           return@Comparator -1
+       }
+       else if (o1.mark > o2.mark) {
+          return@Comparator 1
+       }
+       else {
+           return@Comparator 0
+       }
+    }
+
+     return 0
 }
 
 fun main() {
-   val inp = Scanner(File("in.txt"))
-   val out = PrintStream(FileOutputStream("out.txt"))
+    val inp = Scanner(File("in.txt"))
+    val out = PrintStream(FileOutputStream("out.txt"))
 
-   val n = inp.nextInt()
-   val m = inp.nextInt()
-   val matrix = Array(n) { Array(m) { 0 } }
+    val n = inp.nextInt()
+    val m = inp.nextInt()
+    val matrix = Array(n) { Array(m) { 0 } }
 
-   for(i in 0 until n) {
-      for(j in 0 until m) {
-         val num = inp.nextInt()
-         matrix[i][j] = num
-      }
-   }
+    for (i in 0 until n) {
+        for (j in 0 until m) {
+            val num = inp.nextInt()
+            matrix[i][j] = num
+        }
+    }
 
-   val k = inp.nextInt()
-   val xStart = inp.nextInt()
-   val yStart = inp.nextInt()
-   val xEnd = inp.nextInt()
-   val yEnd = inp.nextInt()
+    val k = inp.nextInt()
+    val xStart = inp.nextInt()
+    val yStart = inp.nextInt()
+    val xEnd = inp.nextInt()
+    val yEnd = inp.nextInt()
 
-   val answer = solution()
+    val answer = solution(n, m, matrix, xStart, yStart, xEnd, yEnd, k)
 
-   out.println(answer)
+    out.println(answer)
 }
