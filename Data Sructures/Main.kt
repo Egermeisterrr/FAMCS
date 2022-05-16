2b29556d470b9ffc88b86ddf4a53b735e9def5ea
Egor <102251036+Egermeisterrr@users.noreply.github.com>
2022-05-16 18:40:50 +0300

    a ready-made solution but with a bug
    ArrayIndexOutOfBoundsException (((

--- Data Sructures/Main.kt
+++ Data Sructures/Main.kt
@@ -2,6 +2,7 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.PrintStream
 import java.util.*
+import kotlin.math.abs
 
 class Vertex(var x: Int, var y: Int, var mark: Int, var height: Int)
 
@@ -26,8 +27,74 @@ fun solution(
            return@Comparator 0
        }
     }
-
-     return 0
+    val pq: PriorityQueue<Vertex> = PriorityQueue<Vertex>(n * m, comparator)
+    var j: Int
+    var u: Int
+    val routes = Array(n + 2) { IntArray(m + 2) }
+    val visited = Array(n + 2) { BooleanArray(m + 2) }
+    for (i in 0 until n + 2) {
+        for (l in 0 until m + 2) {
+            routes[i][l] = Int.MAX_VALUE
+        }
+    }
+    for (i in 0 until n + 2) {
+        visited[i][0] = true
+        visited[i][m + 1] = true
+    }
+    for (i in 0 until m + 1) {
+        visited[0][i] = true
+        visited[n + 1][i] = true
+    }
+    var buf: Vertex
+    pq.add(Vertex(xStart, yStart, 0, matrix[xStart][yStart]))
+    visited[xStart][yStart] = true
+    while (!pq.isEmpty()) {
+        buf = pq.poll()
+        j = buf.x
+        u = buf.y
+        routes[j][u] = buf.mark
+        visited[j][u] = true
+        if (visited[xEnd][yEnd]) {
+            break
+        }
+        if (!visited[j + 1][u] && abs(matrix[j][u] - matrix[j + 1][u]) + k + routes[j][u] < routes[j + 1][u]) {
+            pq.add(
+                Vertex(
+                    j + 1, u, abs(matrix[j][u] - matrix[j + 1][u]) + k + routes[j][u],
+                    matrix[j + 1][u]
+                )
+            )
+            routes[j + 1][u] = abs(matrix[j][u] - matrix[j + 1][u]) + k + routes[j][u]
+        }
+        if (!visited[j - 1][u] && abs(matrix[j][u] - matrix[j - 1][u]) + k + routes[j][u] < routes[j - 1][u]) {
+            pq.add(
+                Vertex(
+                    j - 1, u, abs(matrix[j][u] - matrix[j - 1][u]) + k + routes[j][u],
+                    matrix[j - 1][u]
+                )
+            )
+            routes[j - 1][u] = abs(matrix[j][u] - matrix[j - 1][u]) + k + routes[j][u]
+        }
+        if (!visited[j][u + 1] && abs(matrix[j][u] - matrix[j][u + 1]) + k + routes[j][u] < routes[j][u + 1]) {
+            pq.add(
+                Vertex(
+                    j, u + 1, abs(matrix[j][u] - matrix[j][u + 1]) + k + routes[j][u],
+                    matrix[j][u + 1]
+                )
+            )
+            routes[j][u + 1] = abs(matrix[j][u] - matrix[j][u + 1]) + k + routes[j][u]
+        }
+        if (!visited[j][u - 1] && abs(matrix[j][u] - matrix[j][u - 1]) + k + routes[j][u] < routes[j][u - 1]) {
+            pq.add(
+                Vertex(
+                    j, u - 1, abs(matrix[j][u] - matrix[j][u - 1]) + k + routes[j][u],
+                    matrix[j][u - 1]
+                )
+            )
+            routes[j][u - 1] = abs(matrix[j][u] - matrix[j][u - 1]) + k + routes[j][u]
+        }
+    }
+    return routes[xEnd][yEnd]
 }
 
 fun main() {
